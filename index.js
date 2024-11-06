94bafbf8ed4f76fed7c306a10e9c08e40ae43eb6
Margaret142 <tabbywange75@gmail.com>
2024-11-06 12:37:22 +0300

    completed log in page

---
+++ index.js
@@ -0,0 +1,9 @@
+window.innerWidth = 900;
+window.innerHeight = 600;
+
+const winHeight = window.innerHeight;
+const winWidth = window.innerWidth;
+const aspectRatio = winWidth / winHeight;
+
+
+console.log(winHeight, winWidth, aspectRatio);
